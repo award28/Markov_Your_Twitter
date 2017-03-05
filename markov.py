55bb4763a1e1acfc6ece14a1f9fcc74e463d404b
Ethan Schoen <e.schoen@yahoo.com>
2017-03-05 17:55:16 -0500

    more parsing

--- markov.py
+++ markov.py
@@ -8,8 +8,24 @@ with open(input_text, 'r') as f:
     for line in f.readlines():
         in_str = in_str + line
 f.close()
-remove = ',\':;\"\n'
+in_str = in_str.replace('-\n', '')
+in_str = in_str.replace('\n', ' ')
+remove = '><=+/\\][=+@#$%^&*~`,\':;\"-_'
 for c in remove:
      in_str = in_str.replace(c, '')
-
-print(re.split('([a-zA-Z0-9]*)|([.?!]*)', in_str))
+words = list()
+for w in re.split(r' |([.?!]+)', in_str):
+    if w:
+        words.append(w)
+print(words)
+table = dict()
+for i, w in enumerate(words):
+    if w in table and i < len(table)-1:
+        print(w)
+        table[w].append(words[i+1])
+    else:
+        if i < len(table)-1:
+            table[w] = list(words[i+1])
+        else:
+            table[w] = list()
+print(table)
